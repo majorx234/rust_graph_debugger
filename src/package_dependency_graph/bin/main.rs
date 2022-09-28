685f00f1b383e27c09ffef465ea1936be51d24cb
major <majorx234@googlemail.com>
2022-09-29 01:06:09 +0200

    Add graph creation

--- src/package_dependency_graph/bin/main.rs
+++ src/package_dependency_graph/bin/main.rs
@@ -36,11 +36,30 @@ fn dependency_walker(mut to_check: std::vec::Vec<String>) -> HashMap<String, Opt
     return package_dep_map;
 }
 
+fn create_graph_from_package_dep_list(
+    dep_map: HashMap<String, Option<Vec<String>>>,
+) -> Graph<String, String> {
+    let mut dep_graph: Graph<String, String> = Graph::new();
+    for (package, dependencies_opt) in dep_map {
+        let package_node = dep_graph.add_node(package);
+        match dependencies_opt {
+            Some(dependencies) => {
+                for dep in dependencies {
+                    let dep_node = dep_graph.add_node(dep);
+                    dep_graph.add_edge(package_node, dep_node, "".to_string());
+                }
+            }
+            None => (),
+        }
+    }
+    return dep_graph;
+}
+
 fn main() {
     let packages_vec = read_package_list();
     let packages_count = packages_vec.len();
     println!("packages count: {}", packages_count);
 
     let result = dependency_walker(packages_vec);
-    println!("{:?}", result);
+    let mut dep_graph: Graph<String, String> = create_graph_from_package_dep_list(result);
 }
