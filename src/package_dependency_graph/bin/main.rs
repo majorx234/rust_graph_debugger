120d75f618038685d5cb55c4c4fac83e6caa6473
major <majorx234@googlemail.com>
2022-09-29 00:26:27 +0200

    Simplify pactree wrapper, add depenency walker

--- src/package_dependency_graph/bin/main.rs
+++ src/package_dependency_graph/bin/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 extern crate petgraph;
 mod read_data;
 use crate::read_data::read_package_list;
@@ -6,12 +7,40 @@ use pactree_wrapper::get_deps;
 use petgraph::dot::Dot;
 use petgraph::graph::Graph;
 
+fn dependency_walker(mut to_check: std::vec::Vec<String>) -> HashMap<String, Option<Vec<String>>> {
+    let mut package_dep_map: HashMap<String, Option<Vec<String>>> = HashMap::new();
+    while let Some(package) = to_check.pop() {
+        match package_dep_map.get(&package) {
+            Some(_) => (),
+            None => {
+                let dependencies_opt = get_deps(&package);
+                match dependencies_opt {
+                    Some(ref dependencies) => {
+                        for dep in dependencies {
+                            match package_dep_map.get(dep) {
+                                Some(_) => (),
+                                None => {
+                                    to_check.push(dep.clone());
+                                }
+                            }
+                        }
+                        package_dep_map.insert(package, dependencies_opt);
+                    }
+                    None => {
+                        package_dep_map.insert(package, None);
+                    }
+                }
+            }
+        }
+    }
+    return package_dep_map;
+}
+
 fn main() {
     let packages_vec = read_package_list();
     let packages_count = packages_vec.len();
     println!("packages count: {}", packages_count);
 
-    let packages_dep_vec = get_deps(packages_vec);
-    let packages_dep_vec_count = packages_dep_vec.len();
-    println!("packages dep count: {}", packages_dep_vec_count);
+    let result = dependency_walker(packages_vec);
+    println!("{:?}", result);
 }
--- src/package_dependency_graph/bin/pactree_wrapper.rs
+++ src/package_dependency_graph/bin/pactree_wrapper.rs
@@ -1,29 +1,24 @@
 use std::process::Command;
 
-pub fn get_deps(packages: std::vec::Vec<String>) -> std::vec::Vec<std::vec::Vec<String>> {
+pub fn get_deps(package: &String) -> Option<std::vec::Vec<String>> {
     let mut packages_dep_vec = Vec::new();
-    for package in packages {
-        let mut package_dep_vec: Vec<String> = Vec::new();
-        let output = Command::new("pactree")
-            .arg("-l")
-            .arg("-u")
-            .arg(package)
-            .output()
-            .expect("failed");
 
-        if output.status.success() {
-            let string_output = String::from_utf8(output.stdout).unwrap();
-            let mut dep_packages: Vec<&str> = string_output.split("\n").collect();
-            dep_packages.remove(0);
-            for dep_package in dep_packages {
-                package_dep_vec.push(dep_package.to_string());
-            }
-        } else {
-            package_dep_vec.push("".to_string());
+    let output = Command::new("pactree")
+        .arg("-l")
+        .arg("-u")
+        .arg(&package)
+        .output()
+        .expect("failed");
+
+    if output.status.success() {
+        let string_output = String::from_utf8(output.stdout).unwrap();
+        let mut dep_packages: Vec<&str> = string_output.split("\n").collect();
+        dep_packages.remove(0);
+        for dep_package in dep_packages {
+            packages_dep_vec.push(dep_package.to_string());
         }
-
-        println!("dep size: {}", package_dep_vec.len());
-        packages_dep_vec.push(package_dep_vec);
+        Some(packages_dep_vec)
+    } else {
+        None
     }
-    return packages_dep_vec;
 }
